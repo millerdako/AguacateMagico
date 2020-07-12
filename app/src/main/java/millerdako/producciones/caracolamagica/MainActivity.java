877b67b2176d0a9a3834ee54f48eafc511141e9d
Miller Alfonso Rubio Pérez <millerdako@hotmail.com>
2020-07-12 17:36:26 -0500

    1.1.0 - Incluida publicidad

--- app/src/main/java/millerdako/producciones/caracolamagica/MainActivity.java
+++ app/src/main/java/millerdako/producciones/caracolamagica/MainActivity.java
@@ -10,14 +10,38 @@ import android.widget.ImageView;
 
 import java.util.Random;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer mp;
+    private AdView  mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        mAdView = findViewById(R.id.adView);
+
+        AdView adView = new AdView(this);
+        adView.setAdSize(AdSize.SMART_BANNER);
+        //adView.setAdUnitId("ca-app-pub-3940256099942544/6300978111");
+        adView.setAdUnitId("ca-app-pub-9391440864407001/2773023563");
+
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
 
         Button askButton;
         askButton = (Button) findViewById(R.id.askButton);
